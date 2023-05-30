72ac591da84aa6cb06c02aec18f9a872830b3075
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2023-05-30 11:54:11 +0600

    [Done] setup express mongoose cours parser

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("working successfully!");
+});
+
+export default app;
